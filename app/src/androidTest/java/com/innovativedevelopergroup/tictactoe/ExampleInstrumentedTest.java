bc366df2d94aa01214ed7ceed467e44f10dccdf0
Sourav Ghosh <infosourav04@gmail.com>
2021-06-13 17:55:14 +0530

    Initial Commit

--- app/src/androidTest/java/com/innovativedevelopergroup/tictactoe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/innovativedevelopergroup/tictactoe/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.tictactoe;
+package com.innovativedevelopergroup.tictactoe;
 
 import android.content.Context;
 
--- app/src/main/java/com/innovativedevelopergroup/tictactoe/MainActivity.java
+++ app/src/main/java/com/innovativedevelopergroup/tictactoe/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.tictactoe;
+package com.innovativedevelopergroup.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
