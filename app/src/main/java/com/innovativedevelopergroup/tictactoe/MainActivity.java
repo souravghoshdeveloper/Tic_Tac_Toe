116a03e9b7902d0ac67b588412a8c6f2ad11d89f
Sourav Ghosh <infosourav04@gmail.com>
2021-06-15 08:13:51 +0530

    Initial Commit

--- app/src/main/java/com/innovativedevelopergroup/tictactoe/MainActivity.java
+++ app/src/main/java/com/innovativedevelopergroup/tictactoe/MainActivity.java
@@ -9,15 +9,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     boolean gameActive = true;
-    // Player representation
-    // 0 - X
-    // 1 - O
     int activePlayer = 0;
     int[] gameState = {2, 2 , 2, 2, 2, 2, 2, 2, 2};
-    //    State meanings:
-    //    0 - X
-    //    1 - O
-    //    2 - Null
     int[][] winPositions = {{0,1,2}, {3,4,5}, {6,7,8},
             {0,3,6}, {1,4,7}, {2,5,8},
             {0,4,8}, {2,4,6}};
@@ -43,12 +36,10 @@ public class MainActivity extends AppCompatActivity {
             }
             img.animate().translationYBy(1000f).setDuration(300);
         }
-        // Check if any player has won
         for(int[] winPosition: winPositions){
             if(gameState[winPosition[0]] == gameState[winPosition[1]] &&
                     gameState[winPosition[1]] == gameState[winPosition[2]] &&
                     gameState[winPosition[0]]!=2){
-                // Somebody has won! - Find out who!
                 String winnerStr;
                 gameActive = false;
                 if(gameState[winPosition[0]] == 0){
@@ -57,13 +48,10 @@ public class MainActivity extends AppCompatActivity {
                 else{
                     winnerStr = "O has won";
                 }
-                // Update the status bar for winner announcement
                 TextView status = findViewById(R.id.status);
                 status.setText(winnerStr);
 
             }
-
-
 
         }
 
